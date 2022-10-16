544fd3fb177cf74a735799842374882016d548bb
Jorge Quintana García <alu0101123547@ull.edu.es>
2022-10-16 16:44:07 +0100

    TriggerTry.cs

---
+++ TriggerTry.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTry : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+      Rigidbody rb = other.attachedRigidbody;
+      if (rb != null) {
+        other.transform.position = transform.position + new Vector3(0, 4, 0);
+        other.attachedRigidbody.velocity = new Vector3(0, 0, 0);
+      }
+    }
+}
